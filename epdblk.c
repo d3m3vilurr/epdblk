d4c76e33f248387426bb02d968a5a9431d382a4a
Sunguk Lee <d3m3vilurr@gmail.com>
2018-03-11 00:49:16 +0900

    Introduce error enums and stub code detection fb types

--- epdblk.c
+++ epdblk.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <fcntl.h>
+#include <string.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <sys/stat.h>
@@ -8,13 +9,24 @@
 
 #define FB_DEV "/dev/graphics/fb0"
 
+enum {
+    NO_ERROR            = 0x00,
+    ERROR_INV_ARGS      = 0x01,
+    ERROR_OPEN_FB       = 0x02,
+    ERROR_GET_FINFO     = 0x10,
+    ERROR_GET_VINFO     = 0x11,
+    ERROR_WRONG_FB      = 0x20,
+    ERROR_FULL_REFRESH  = 0x30,
+};
+
 static char *fb_devs[] = {
     "/dev/graphics/fb0",
     "/dev/fb0",
     NULL,
 };
 
-static struct fb_var_screeninfo fb_info;
+static struct fb_fix_screeninfo fb_fix_info;
+static struct fb_var_screeninfo fb_var_info;
 
 struct update_payload_t {
     uint32_t top;
@@ -46,10 +58,10 @@ int close_framebuffer(int fb) {
 }
 
 int main(int argc, char **argv) {
-    int ret = 0;
+    uint8_t ret = NO_ERROR;
 
     if (argc < 2) {
-        ret = -1;
+        ret = ERROR_INV_ARGS;
         goto exit;
     }
 
@@ -58,20 +70,30 @@ int main(int argc, char **argv) {
 
     int fb = open_framebuffer();
     if (fb < 0) {
-        ret = -2;
+        ret = ERROR_OPEN_FB;
         goto exit;
     }
 
-    if (ioctl(fb, FBIOGET_VSCREENINFO, &fb_info) < 0) {
-        ret = -3;
+    if (ioctl(fb, FBIOGET_FSCREENINFO, &fb_fix_info) < 0) {
+        ret = ERROR_GET_FINFO;
+        goto cleanup;
+    }
+
+    if (strcmp(fb_fix_info.id, "imx_epdc") != 0) {
+        ret = ERROR_WRONG_FB;
+        goto cleanup;
+    }
+
+    if (ioctl(fb, FBIOGET_VSCREENINFO, &fb_var_info) < 0) {
+        ret = ERROR_GET_VINFO;
         goto cleanup;
     }
 
     struct update_payload_t payload = {
         .top = 0,
         .left = 0,
-        .width = fb_info.xres,
-        .height = fb_info.yres,
+        .width = fb_var_info.xres,
+        .height = fb_var_info.yres,
         .waveform_mode = 0x101,
         .update_mode = 1,
         .temp = 0x18,
@@ -79,7 +101,7 @@ int main(int argc, char **argv) {
     };
 
     if (ioctl(fb, 0x4040462E, &payload) < 0) {
-        ret = -4;
+        ret = ERROR_FULL_REFRESH;
         goto cleanup;
     }
 
