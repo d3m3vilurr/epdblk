e85549e0618960277e7c4971489f63cbc613de0c
Sunguk Lee <d3m3vilurr@gmail.com>
2018-03-11 01:25:26 +0900

    More cleanup

--- epdblk.c
+++ epdblk.c
@@ -28,18 +28,6 @@ static char *fb_devs[] = {
 static struct fb_fix_screeninfo fb_fix_info;
 static struct fb_var_screeninfo fb_var_info;
 
-struct update_payload_t {
-    uint32_t top;
-    uint32_t left;
-    uint32_t width;
-    uint32_t height;
-    uint32_t waveform_mode;
-    uint32_t update_mode;
-    uint32_t update_marker;
-    uint32_t temp;
-    uint32_t flags;
-};
-
 int open_framebuffer() {
     int fb;
     int i = 0;
@@ -57,8 +45,61 @@ int close_framebuffer(int fb) {
     close(fb);
 }
 
+#define MXCFB_SEND_UPDATE 0x4040462E
+enum {
+    WAVEFORM_MODE_AUTO  = 0x101,
+};
+
+enum {
+    UPDATE_MODE_PARTIAL = 0x0,
+    UPDATE_MODE_FULL    = 0x1,
+};
+
+struct mxcfb_rect {
+    uint32_t top;
+    uint32_t left;
+    uint32_t width;
+    uint32_t height;
+};
+
+struct mxcfb_alt_buffer_data {
+    uint32_t phys_addr;
+    uint32_t width;
+    uint32_t height;
+    struct mxcfb_rect alt_update_region;
+};
+
+struct mxcfb_update_data {
+    struct mxcfb_rect update_region;
+    uint32_t waveform_mode;
+    uint32_t update_mode;
+    uint32_t update_marker;
+    int32_t temp;
+    uint32_t flags;
+    struct mxcfb_alt_buffer_data alt_buffer_data;
+};
+
+int refresh_imx(int fb) {
+
+    if (ioctl(fb, FBIOGET_VSCREENINFO, &fb_var_info) < 0) {
+        return ERROR_GET_VINFO;
+    }
+
+    struct mxcfb_update_data update_data = {0};
+    update_data.update_region.width = fb_var_info.xres;
+    update_data.update_region.height = fb_var_info.yres;
+    update_data.waveform_mode = WAVEFORM_MODE_AUTO;
+    update_data.update_mode = UPDATE_MODE_FULL;
+    update_data.temp = 0x18;
+
+    if (ioctl(fb, MXCFB_SEND_UPDATE, &update_data) < 0) {
+        return ERROR_FULL_REFRESH;
+    }
+    return NO_ERROR;
+}
+
 int main(int argc, char **argv) {
-    uint8_t ret = NO_ERROR;
+    uint8_t ret;
 
     if (argc < 2) {
         ret = ERROR_INV_ARGS;
@@ -79,30 +120,10 @@ int main(int argc, char **argv) {
         goto cleanup;
     }
 
-    if (strcmp(fb_fix_info.id, "imx_epdc") != 0) {
+    if (strcmp(fb_fix_info.id, "imx_epdc") == 0) {
+        ret = refresh_imx(fb);
+    } else {
         ret = ERROR_WRONG_FB;
-        goto cleanup;
-    }
-
-    if (ioctl(fb, FBIOGET_VSCREENINFO, &fb_var_info) < 0) {
-        ret = ERROR_GET_VINFO;
-        goto cleanup;
-    }
-
-    struct update_payload_t payload = {
-        .top = 0,
-        .left = 0,
-        .width = fb_var_info.xres,
-        .height = fb_var_info.yres,
-        .waveform_mode = 0x101,
-        .update_mode = 1,
-        .temp = 0x18,
-        .flags = 0,
-    };
-
-    if (ioctl(fb, 0x4040462E, &payload) < 0) {
-        ret = ERROR_FULL_REFRESH;
-        goto cleanup;
     }
 
 cleanup:
